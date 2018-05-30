2b465a03c57cad12022947e79a13c6b7ed3c35f6
Sussman, Allison <asussman@usgs.gov>
2018-05-30 14:30:03 -0400

    added modal to female ducks on pg2, cleaned up a little

--- js/tooltip.js
+++ js/tooltip.js
@@ -8,41 +8,20 @@ $(document).ready(function(){
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
+var img = $('.myImg');
+var modalImg = $("#img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
+$('.myImg').click(function(){
     modal.style.display = "block";
-    modalImg.src = this.src;
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
     captionText.innerHTML = this.alt;
-}
+});
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-    modal.style.display = "none";
-};
-
-//modal script 2
-// Get the modal
-var modal = document.getElementById('myModal2');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg2');
-var modalImg = document.getElementById("img02");
-var captionText = document.getElementById("caption2");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+span.onclick = function() {
+  modal.style.display = "none";
 }
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close2")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-    modal.style.display = "none";
-};
